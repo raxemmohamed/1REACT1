81b63e1412182dab10cbf6a43f4170e729d7c194
raxemmohamed <raxemmohamed@gmail.com>
2020-01-01 14:29:46 -0600

    200 from postman backend

---
+++ config/keys.js
@@ -0,0 +1,3 @@
+module.exports = {
+    mongoURI: 'mongodb+srv://raxem:raxem123@mern-woqtj.mongodb.net/test?retryWrites=true&w=majority'
+}
---
+++ routes/api/items.js
@@ -0,0 +1,17 @@
+const express = require('express');
+const router = express.Router();
+
+//item model
+const Item = require('../../models/Item');
+
+//@route POST api/items
+//@desc create a post
+//@access public
+router.post('/', (req, res) => {
+    const newItem = new Item({
+        name: req.body.name
+    });
+    newItem.save().then(item => res.json(item));
+});
+
+module.exports = router;
